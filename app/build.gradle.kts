7be1f7c028857aeafeb78652b218477aafc7451f
Evgeniy Krasnyanskiy <Evgeniy.Krasnyanskiy@gmail.com>
2026-09-09 01:48:06 +0300

    feat: initialize build.gradle.kts with Android application configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ android {
             excludes += "/META-INF/*.kotlin_module"
             excludes += "DebugProbesKt.bin"
             excludes += "kotlin-tooling-metadata.json"
+            
 			excludes += "META-INF/com.android.tools/**"
             excludes += "META-INF/proguard/**"
             excludes += "/*.proto"
